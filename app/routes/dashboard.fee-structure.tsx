6f0f6c785daaf6db9aac1c35d00b00339de2c050
= <=>
2024-04-29 21:48:41 +0530

    fee,transport and timetable route added

---
+++ app/routes/dashboard.fee-structure.tsx
@@ -0,0 +1,9 @@
+export default function FeeStructure() {
+  return (
+    <main className="flex-grow p-6">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-lg font-medium text-white">Fee Structure</h1>
+      </div>
+    </main>
+  );
+}
