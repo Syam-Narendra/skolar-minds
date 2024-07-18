a59933d098538a3480899493791b9b22d8c06088
Syam Narendra <syamnarendra85@gmail.com>
2024-07-19 00:38:35 +0530

    signup success page added

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,56 @@
-import React from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
-
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 const SigninPage = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<{ email: string; password: string }>();
-  const onSubmit: SubmitHandler<{ email: string; password: string }> = (data) => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<{ loginEmail: string; loginPassword: string }>();
+  const [responseError, setResponseError] = useState<{
+    colour: string;
+    message: string;
+  }>({ colour: "", message: "" });
+  const [isLoading, setLoading] = useState(false);
+  const onSubmit: SubmitHandler<{
+    loginEmail: string;
+    loginPassword: string;
+  }> = async (data) => {
+    setLoading(true);
+    const response = await fetch("http://localhost:3000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const result = await response.json();
+    setLoading(false);
+    if (response.status === 202) {
+      setResponseError({
+        colour: "text-yellow-500",
+        message: result.message,
+      });
+    }
+    if (response.status === 404 || response.status === 401) {
+      setResponseError({
+        colour: "text-red-500",
+        message: result.message,
+      });
+    }
+  };
   const formFields = [
-    { name: 'email', label: 'Email', type: 'email', placeholder: "Enter your email" },
-    { name: 'password', label: 'Password', type: 'password', placeholder: "Enter your password" }
+    {
+      name: "loginEmail",
+      label: "Email",
+      type: "email",
+      placeholder: "Enter your email",
+    },
+    {
+      name: "loginPassword",
+      label: "Password",
+      type: "password",
+      placeholder: "Enter your password",
+    },
   ];
 
   return (
@@ -25,6 +69,7 @@ const SigninPage = () => {
         <form
           onSubmit={handleSubmit(onSubmit)}
           className="grid grid-cols-1 gap-3 pt-5 pb-5"
+          onChange={() => setResponseError({ colour: "", message: "" })}
         >
           {formFields.map((field) => (
             <div key={field.name} className="flex flex-col">
@@ -34,28 +79,33 @@ const SigninPage = () => {
               <input
                 placeholder={field.placeholder}
                 id={field.name}
-                {...register(field.name as "email" | "password", {
+                {...register(field.name as "loginEmail" | "loginPassword", {
                   required: `${field.label} is required`,
                 })}
                 type={field.type}
                 className="p-2 rounded-lg bg-transparent border-2 border-[#70707B] text-white focus:outline-none focus:ring-2 focus:ring-[#1877F2] focus:border-transparent"
               />
-              {errors[field.name as "email" | "password"] ? (
+              {errors[field.name as "loginEmail" | "loginPassword"] && (
                 <span className="text-red-500 text-sm mt-2">
                   {errors[
-                    field.name as "email" | "password"
+                    field.name as "loginEmail" | "loginPassword"
                   ]?.message?.toString()}
                 </span>
-              ): <span className='text-sm mt-2 text-transparent'>.</span>}
+              )}
             </div>
           ))}
-          <div className="flex justify-center">
-            <button
-              type="submit"
-              className="w-full p-2 bg-[#1877F2] text-white rounded mt-3 transition duration-200"
-            >
-              Sign In
-            </button>
+          <p className={responseError.colour}>{responseError.message}</p>
+          <div className="flex flex-col items-center justify-center">
+            {isLoading ? (
+              <div className="loader"></div>
+            ) : (
+              <button
+                type="submit"
+                className="w-full p-2 bg-[#1877F2] text-white rounded mt-3 transition duration-200"
+              >
+                Sign In
+              </button>
+            )}
           </div>
         </form>
         <div className="flex justify-between ">
--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -17,18 +17,24 @@ interface FormFields {
   confirmPassword: string;
 }
 
-const SignUpPage = () => {
+const Form = ({
+  setShowForm,
+}: {
+  setShowForm: (showForm: boolean) => void;
+}) => {
   const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState(false);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<FormFields>();
-  const onSubmit: SubmitHandler<FormFields> = async(formData) =>{
+  const onSubmit: SubmitHandler<FormFields> = async (formData) => {
     setError("");
-
+    setLoading(true);
     if (formData.password !== formData.confirmPassword) {
       setError("Passwords do not match");
+      setLoading(false);
     } else {
       try {
         const { data, status } = await axios.post(
@@ -37,21 +43,20 @@ const SignUpPage = () => {
         );
         if (status === 200) {
           setError("");
+          setShowForm(false);
           console.log(data);
           console.log(formData);
-        }
-        else{
+        } else {
           setError("Something went wrong");
         }
       } catch (error) {
         const err = error as AxiosError;
         const errorMsg = err.response?.data as { message: string };
         setError(errorMsg.message);
+        setLoading(false);
       }
     }
-  }
-    
-
+  };
 
   const formFields = [
     { name: "name", label: "Your Name", type: "text" },
@@ -70,6 +75,102 @@ const SignUpPage = () => {
   ];
 
   return (
+    <div className=" relative bg-[#151515] bg-opacity-30 backdrop-blur-xl p-6 rounded-lg shadow-[0_-4px_10px_rgba(255,255,255,0.1)] w-full max-w-fit">
+      <h2 className="text-2xl font-bold text-center text-white mb-2">
+        Start Your 7 Day Free Trial !
+      </h2>
+      <form
+        onChange={() => {
+          setError("");
+          setLoading(false);
+        }}
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid grid-cols-1 md:grid-cols-3 gap-4 pt-5 pb-5"
+      >
+        {formFields.map((field) => (
+          <div key={field.name} className="flex flex-col">
+            <label htmlFor={field.name} className="text-white">
+              {field.label}
+            </label>
+            <input
+              autoComplete="off"
+              id={field.name}
+              {...register(field.name as keyof FormFields, {
+                required: `${field.label} is required`,
+              })}
+              type={field.type}
+              className={`p-2 rounded-lg bg-transparent border-2  border-[#70707B] text-white focus:outline-none focus:ring-2 focus:ring-[#1877F2] focus:border-transparent`}
+            />
+            {errors[field.name as keyof FormFields] ? (
+              <span className="text-red-500 text-sm mt-2">
+                {errors[field.name as keyof FormFields]?.message?.toString()}
+              </span>
+            ) : (
+              <span className=" text-sm mt-2 text-transparent">.</span>
+            )}
+          </div>
+        ))}
+        <div className=" flex flex-col items-center justify-center md:col-span-full gap-3">
+          {error && <span className="text-red-500 text-sm">{error}</span>}
+          {loading ? (
+            <div className="loader"></div>
+          ) : (
+            <button
+              type="submit"
+              className=" p-2 bg-[#1877F2] text-white rounded transition duration-200"
+            >
+              Sign Up For Verification
+            </button>
+          )}
+
+          <a
+            href="/"
+            className="text-[#70707B]  hover:underline text-sm text-center md:col-span-2"
+          >
+            Already have an account? Sign In
+          </a>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+const SuccessMessage = () => {
+  return (
+    <div className="relative bg-[#151515] flex flex-col bg-opacity-30 backdrop-blur-xl p-8 rounded-lg shadow-[0_-4px_10px_rgba(255,255,255,0.1)] max-w-md">
+      <p className="text-center text-[#70707B] mb-2">Welcome To Skolar Minds</p>
+      <h2 className="text-xl font-bold text-center text-white mb-6">
+        Account Created Successfully!
+      </h2>
+      <img
+        className="w-28 h-28 object-cover rounded-full self-center"
+        src="https://i.ibb.co/mCFDxwH/tick-icon.png"
+      />
+      <p className="text-green-600 text-center mt-4">
+        🎉 Welcome aboard! Your account has been successfully created.
+        <br />{" "}
+        <span className="text-white">
+          You will be able to log in once we verify your account. We will notify
+          you once the verification is complete.
+        </span>
+      </p>
+
+      <div className="flex justify-center text-center mt-4 ">
+        <p className="text-[#70707B] text-sm">
+          Click Here To Sign In
+          <br />
+          <a className="text-[#70707B] hover:underline" href="/">
+            Sign In
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const SignUpPage = () => {
+  const [showForm, setShowForm] = useState(false);
+  return (
     <div className="relative min-h-screen p-10 flex items-center justify-center">
       <div
         className="absolute inset-0 bg-cover bg-center"
@@ -77,55 +178,7 @@ const SignUpPage = () => {
       >
         <div className="absolute inset-0 bg-black opacity-50"></div>
       </div>
-      <div className=" relative bg-[#151515] bg-opacity-30 backdrop-blur-xl p-6 rounded-lg shadow-[0_-4px_10px_rgba(255,255,255,0.1)] w-full max-w-fit">
-        <h2 className="text-2xl font-bold text-center text-white mb-2">
-          Start Your 7 Day Free Trial !
-        </h2>
-        <form
-          onChange={() => setError("")}
-          onSubmit={handleSubmit(onSubmit)}
-          className="grid grid-cols-1 md:grid-cols-3 gap-4 pt-5 pb-5"
-        >
-          {formFields.map((field) => (
-            <div key={field.name} className="flex flex-col">
-              <label htmlFor={field.name} className="text-white">{field.label}</label>
-              <input
-                autoComplete="off"
-                id={field.name}
-                {...register(field.name as keyof FormFields, {
-                  required: `${field.label} is required`,
-                })}
-                type={field.type}
-                className={`p-2 rounded-lg bg-transparent border-2  border-[#70707B] text-white focus:outline-none focus:ring-2 focus:ring-[#1877F2] focus:border-transparent`}
-              />
-              {errors[field.name as keyof FormFields] ? (
-                <span className="text-red-500 text-sm mt-2">
-                  {errors[field.name as keyof FormFields]?.message?.toString()}
-                </span>
-              ) : (
-                <span className=" text-sm mt-2 text-transparent">
-                  .
-                </span>
-              )}
-            </div>
-          ))}
-          <div className=" flex flex-col items-center justify-center md:col-span-full gap-3">
-            {error && <span className="text-red-500 text-sm">{error}</span>}
-            <button
-              type="submit"
-              className=" p-2 bg-[#1877F2] text-white rounded transition duration-200"
-            >
-              Sign Up For Verification
-            </button>
-            <a
-              href="/"
-              className="text-[#70707B]  hover:underline text-sm text-center md:col-span-2"
-            >
-              Already have an account? Sign In
-            </a>
-          </div>
-        </form>
-      </div>
+      {showForm ? <Form setShowForm={setShowForm} /> : <SuccessMessage />}
     </div>
   );
 };
