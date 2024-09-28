e4b08feb86035d75978dd0f4a134f309d22614cd
Syam <syamnarendra85@gmail.com>
2024-09-29 01:50:23 +0530

    loader page added

---
+++ app/customComponents/LoadingPage.tsx
@@ -0,0 +1,11 @@
+import { Loader2 } from "lucide-react";
+
+export default function LoadingPage() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen bg-black">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500">
+        <Loader2 className="h-16 w-16 text-blue-500" />
+      </div>
+    </div>
+  );
+}
